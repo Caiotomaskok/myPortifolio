cdca7d25ca29373057300ed30730fea7ff5a9aee
Caio Ricci <caiotomaskok078@gmail.com>
2023-07-27 14:50:05 -0300

    feat: add hover and links in menu Bar

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -1,21 +1,51 @@
+import { Route, Routes } from 'react-router-dom'
+
 import { MdAccountBox  } from 'react-icons/md'
 import { BsTelephoneFill } from 'react-icons/bs'
-import { } from 'react-icons'
+import { BsFillRocketFill } from 'react-icons/bs'
+import { GiUnstableProjectile } from 'react-icons/gi'
+import { FaSun } from 'react-icons/fa'
+import { MdOutlineMenuOpen } from 'react-icons/md'
 import './MenuBar.scss'
+import HomePage from '../pages/HomePage'
 
 const MenuBar = () => {
+
+function handleChangeSetup() {
+    return(
+        alert('teste')
+    )
+}
+
  return (
     <div className='menu-bar-content'>
+       
         <div className="list-itens">
-            <div className="item1">
-                <MdAccountBox size={35}/>
-                </div>
-            <div className="item1">
-                <BsTelephoneFill size={30}/>
+            
+             <div className="item1">
+                <div className='icon-menu'> <MdOutlineMenuOpen size={30}/></div>
             </div>
-            <div className="item1">Habilidades</div>
-            <div className="item1">Sobre mim</div>
+            <div className="item1">
+                <div className='icon-sun'> <FaSun size={30}/></div>
+            </div>
+            <div className="item1">
+                <div className="icon-about"><MdAccountBox size={20}/></div>
+                <span className='add'>Sobre</span>
+                </div>
+            <div className="item1" >
+               <div className="icon-conta"><BsTelephoneFill size={20}/></div> 
+               <span>Contato</span>
+            </div>
+            <div className="item1" onclick={handleChangeSetup}>
+                <div className="icon-skills"><BsFillRocketFill size={20}/></div>
+                <span>Habilidades</span>
+            </div>
+            <div className="item1">
+                <div className='icon-project'> <GiUnstableProjectile size={20}/></div>
+                <span>Projetos</span>
+            </div>
         </div>
+        
     </div>
  )
 }
